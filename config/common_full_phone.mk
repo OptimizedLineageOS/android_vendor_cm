7839151271716c203f493fb7e89aae28ace17600
Arne Coucheron <arco68@gmail.com>
2016-09-09 00:04:09 +0200

    cm: Minor config reorganization
     * Move extra tools to common.mk. These are needed for CMFM, and should
       be included for all.
     * Move LiveWallpapersPicker, SoundRecorder and Screencast
       to common.mk. These apps are extremely small, and only takes up
       less than 1 MB combined.
     * Move sound property overrides to common.mk. There's no need
       to have them spread across several files.
     * Include CM audio files from common.mk
    
    Change-Id: If308506cd9cd0a75014de988c242d700db5d16f3
    
    Conflicts:
    	config/common_full.mk
    	config/common_full_phone.mk

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -8,11 +8,6 @@ PRODUCT_PACKAGES += \
 # Include CM LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
 
-# Default notification/alarm sounds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Tethys.ogg \
-    ro.config.alarm_alert=Oxygen.ogg
-
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
         vendor/cm/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
